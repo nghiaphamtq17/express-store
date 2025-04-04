fcb54e6f9b35dfa812bc874d91f2572f93e121c5
Your Name <iamcodertq17@gmail.com>
2025-04-04 10:03:48 +0700

    update

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,63 @@
-var express = require('express');
+var express = require("express");
+const User = require("../model/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/list', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.post("/register", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    // Kiểm tra user đã tồn tại chưa
+    const existingUser = await User.findOne({ $or: [{ email }, { username }] });
+    if (existingUser)
+      return res
+        .status(400)
+        .json({ message: "username hoặc email đã được sử dụng" });
+
+    // Mã hóa mật khẩu
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Tạo user mới
+    const newUser = new User({ username, email, password: hashedPassword });
+    await newUser.save();
+
+    res.status(201).json({ message: "Đăng ký thành công" });
+  } catch (err) {
+    res.status(500).json({ message: "Lỗi server" });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // Tìm user theo username
+    const user = await User.findOne({ username });
+    if (!user)
+      return res.status(400).json({ message: "Tài khoản không tồn tại" });
+
+    // Kiểm tra mật khẩu
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Mật khẩu không đúng" });
+
+    // Tạo JWT token
+    const token = jwt.sign({ userId: user._id }, "SECRET_KEY", {
+      expiresIn: "7d",
+    });
+
+    const {password:PU , ...result} = user?._doc;
+
+    res.json({ message: "Đăng nhập thành công", token, user: result });
+  } catch (err) {
+    res.status(500).json({ message: "Lỗi server" });
+  }
 });
 
 module.exports = router;
